f161fb8974516ba6ba017445d4be76de6defb155
Gabriel Matute <orangedev8@gmail.com>
2024-12-26 19:51:47 -0600

    changes for cors

--- mest-backend/src/main.ts
+++ mest-backend/src/main.ts
@@ -1,24 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-let allowCrossDomain = function (_req: any, res: any, next: any) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'x-api-key,authorization,Cache-Control,content-type',
-  );
-
-  next();
-};
-
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  app.use(allowCrossDomain);
+  const app = await NestFactory.create(AppModule);
+
   app.enableCors({
     origin: ['http://localhost:3000'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
+    allowedHeaders: [
+      'x-api-key',
+      'authorization',
+      'Cache-Control',
+      'content-type',
+    ],
   });
 
   await app.listen(8000);
