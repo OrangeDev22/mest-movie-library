2ebb648d13b36e6f962e82b3634ce624117117c8
Gabriel Matute <orangedev8@gmail.com>
2024-12-27 01:16:31 -0600

    fixing style issues for sign in button

--- mest-frontend/src/app/components/SiginButtons/index.tsx
+++ mest-frontend/src/app/components/SiginButtons/index.tsx
@@ -4,8 +4,10 @@ import Button from "../Button";
 
 function SiginButtons() {
   return (
-    <Link href={"/api/auth/login"}>
-      <Button>Sign in</Button>
+    <Link href={"/api/auth/login"} className="min-w-[54px] md:max-w-auto">
+      <Button className="px-2 py-1 md:px-4 md:py-2 text-xs sm:text-sm md:text-base w-full">
+        Sign in
+      </Button>
     </Link>
   );
 }
