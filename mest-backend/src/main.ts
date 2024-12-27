6c32944efa585aaf21dd46cd2b8aec79ce6fcbd8
Gabriel Matute <orangedev8@gmail.com>
2024-12-26 19:48:25 -0600

    adding cors permission

--- mest-backend/src/main.ts
+++ mest-backend/src/main.ts
@@ -15,7 +15,11 @@ let allowCrossDomain = function (_req: any, res: any, next: any) {
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.use(allowCrossDomain);
-  app.enableCors();
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
   await app.listen(8000);
 }
