6d04a5aaa8c4298806cb627ee099f689413ffc91
Gabriel Matute <orangedev8@gmail.com>
2025-01-06 19:20:31 -0600

    fixing settings user picture in mobile devices

--- mest-frontend/src/app/components/UserCard/index.tsx
+++ mest-frontend/src/app/components/UserCard/index.tsx
@@ -15,16 +15,18 @@ const UserCard = ({ user, isLoading, onButtonClick }: UserCardProps) => {
     <div className="p-4 max-w-3xl mx-auto bg-base-100  text-white rounded-lg shadow-md flex flex-col gap-4">
       <div className="flex items-center gap-6">
         {/* User profile picture */}
-        <div className="flex gap-3 items-center w-full">
-          <div className="rounded-full overflow-hidden border-2 border-gray-500">
-            <UserPicture size="md" />
+        <div className="flex gap-3 flex-col sm:flex-row items-start sm:items-center w-full">
+          <div className="flex gap-3 items-center">
+            <div className="rounded-full overflow-hidden border-2 border-gray-500">
+              <UserPicture size="md" />
+            </div>
+            <div className="">
+              <div className="text-2xl font-semibold">{user?.name}</div>
+            </div>
           </div>
-          <div className="">
-            <div className="text-2xl font-semibold">{user?.name}</div>
-          </div>
-          <div className="text-center ml-auto">
+          <div className="text-center ml-auto w-full sm:w-auto">
             <Button
-              className="text-white rounded-lg font-semibold w-90"
+              className="text-white rounded-lg font-semibold w-full sm:w-90"
               onClick={onButtonClick}
             >
               Edit Profile
